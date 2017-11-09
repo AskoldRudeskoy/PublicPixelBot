d443bdda50961f1dc2ffc5410002d07a86079106
v14d1514v <33500755+v14d1514v@users.noreply.github.com>
2017-11-10 00:23:13 +0300

    Update PixelBot.user.js

--- PixelBot.user.js
+++ PixelBot.user.js
@@ -15,7 +15,7 @@ function CoiPixelBot() {
 
     CoiPixelBot.url = {
         script: 'https://raw.githubusercontent.com/v14d1514v/PublicPixelBot/blob/master/PixelBot.user.js',
-        image:  'https://i.imgur.com/7ab3dN6.png'
+        image:  'https://i.imgur.com/wd1Y44K.png'
     };
 
     CoiPixelBot.refreshTime = 300;
